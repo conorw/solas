b60220aa16f8cc763097cd40ebe50f9ff58fb50e
Conor Woods <conorw@gmail.com>
2022-12-15 13:00:25 +0000

    public env vars

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { env } from '$env/dynamic/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 
-console.log('env.PUBLIC_SUPABASE_URL', env.PUBLIC_SUPABASE_URL);
-export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+
+console.log('PUBLIC_SUPABASE_URL', PUBLIC_SUPABASE_URL);
+export const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
